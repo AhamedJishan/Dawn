885598bac5b0e9f3bde35754b74446e2e6d5a839
Jishan Ahamed <146475810+AhamedJishan@users.noreply.github.com>
2026-02-27 17:07:28 +0530

    Physics.h added (Sphere and Ray structs)

---
+++ Engine/src/Physics/Physics.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <glm/vec3.hpp>
+
+namespace Dawn
+{
+	namespace Physics
+	{
+		struct Ray
+		{
+			glm::vec3 origin = glm::vec3(0);
+			glm::vec3 direction = glm::vec3(0, 0, -1);;
+		};
+
+		struct Sphere
+		{
+			glm::vec3 center = glm::vec3(0);
+			float radius = 1.0f;
+		};
+	}
+}
